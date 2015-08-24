6ee0ff349b12a484db84507b24b2b60c88b6c1c0
Alex Bannon <alexbannon@gmail.com>
2015-08-24 16:22:04 -0400

    changed map height

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  var lat     = 51.5072;
-  var long    = -0.1275;
+  var lat     = 13.5333;
+  var long    = 2.0833;
 
-  L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw';
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYWxleGJhbm5vbiIsImEiOiIzM2I3MWU4NjhlNjc5ODYzN2NjMWFhYzU4OWIzOGYzYiJ9.zVY-I01f5Pie1XCaA0Laog';
   // Create a map in the div #map
-  var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 13);
+  var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 3);
     L.marker([lat, long], {
       icon: L.mapbox.marker.icon({
         'marker-size': 'large',
