e4d26ff5c8a42a66d15f62161eb8aac3bfea7c93
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-24 16:03:05 -0400

    adding a commit after merge conflict

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
+  var lat     = 51.5072;
+  var long    = -0.1275;
+
+  L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw';
+  // Create a map in the div #map
+  var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 13);
+    L.marker([lat, long], {
+      icon: L.mapbox.marker.icon({
+        'marker-size': 'large',
+        'marker-symbol': 'camera',
+        'marker-color': '#fa0'
+      })
+    }).addTo(map);
+  var cir = L.circle([lat, long], 1000).addTo(map);
+
+  map.scrollWheelZoom.disable();
+
+
+
   User.fetch().then(function(users){
     users.forEach(function(user){
       $(".users").append(user.username)
     })
   })
-  L.mapbox.accessToken = 'pk.eyJ1IjoiYWxleGJhbm5vbiIsImEiOiIzM2I3MWU4NjhlNjc5ODYzN2NjMWFhYzU4OWIzOGYzYiJ9.zVY-I01f5Pie1XCaA0Laog';
-  var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([40, -74.50], 9);
 
-})
+
+});
