308145671cdc31da56179ea558cf8f563a41aff7
Alex Bannon <alexbannon@gmail.com>
2015-08-24 15:30:22 -0400

    starting mapbox api

--- public/js/script.js
+++ public/js/script.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
       $(".users").append(user.username)
     })
   })
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYWxleGJhbm5vbiIsImEiOiIzM2I3MWU4NjhlNjc5ODYzN2NjMWFhYzU4OWIzOGYzYiJ9.zVY-I01f5Pie1XCaA0Laog';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([40, -74.50], 9);
+
 })
