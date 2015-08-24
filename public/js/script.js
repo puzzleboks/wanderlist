0263d6486fa416b729d7d99d456ad087098fe820
lon phelps <puzzleboks@gmail.com>
2015-08-24 18:16:45 -0400

    added geojson green and red pins

--- public/js/script.js
+++ public/js/script.js
@@ -4,19 +4,62 @@ $(document).ready(function(){
 
   L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw';
   // Create a map in the div #map
-  var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 13);
-    L.marker([lat, long], {
-      icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-symbol': 'camera',
-        'marker-color': '#fa0'
-      })
-    }).addTo(map);
-  var cir = L.circle([lat, long], 1000).addTo(map);
+  var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 3);
+  var myLayer = L.mapbox.featureLayer().addTo(map);
 
-  map.scrollWheelZoom.disable();
+var geoJson = [
+  {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.00, 40]
+    },
+    "properties": {
+        "title": "red pin",
+        "icon": {
+            "iconUrl": "../public/images/PinDown1.png",
+            "iconSize": [22, 27], // size of the icon
+            "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+            "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+            "className": "dot"
+        }
+    }
+},{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates": [51.5072, -0.1275]
+  },
+  "properties": {
+      "title": "green pin",
+      "icon": {
+          "iconUrl": "../public/images/PinDown1Green.png",
+          "iconSize": [22, 27], // size of the icon
+          "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+          "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+          "className": "dot"
+      }
+  }
+}
 
+];
 
+// Set a custom icon on each marker based on feature properties.
+myLayer.on('layeradd', function(e) {
+    var marker = e.layer,
+        feature = marker.feature;
+
+    marker.setIcon(L.icon(feature.properties.icon));
+});
+
+myLayer.on("click", function(){
+  console.log("click")
+})
+
+// Add features to the map.
+myLayer.setGeoJSON(geoJson);
+
+  map.scrollWheelZoom.enable();
 
   User.fetch().then(function(users){
     users.forEach(function(user){
