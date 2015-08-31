0888ad349cb4dd4e8376aa398d6260f51f2594fb
Alex Bannon <alexbannon@gmail.com>
2015-08-31 18:39:04 -0400

    removed final localhost and added https to links

--- public/js/views/photoView.js
+++ public/js/views/photoView.js
@@ -1,6 +1,6 @@
 var PhotoView = function(pinId, photoId){
   var self = this;
-  $.getJSON("http://localhost:3000/pins/"+pinId+"/photos/"+photoId)
+  $.getJSON("/pins/"+pinId+"/photos/"+photoId)
   .done(function(response){
     self.photoUrl = response.photoUrl;
     self.pinId = response.pinId;
