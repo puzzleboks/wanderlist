eb41d4f1bfdc54c6cab62cb4063edd894c9e9d8c
Alex Bannon <alexbannon@gmail.com>
2015-08-31 16:43:38 -0400

    changed connection file

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,18 @@
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize("postgres:///wanderlist");
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize("postgres:///playlistr_db");
+}
+
 var User = sequelize.import("../models/user");
 var Pin = sequelize.import("../models/pin");
 var Photo = sequelize.import("../models/photo");
